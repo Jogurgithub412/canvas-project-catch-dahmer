c9ed784fad11137b535183786c965a8da0e6f8fb
Renatopais <ren.tomar@gmail.com>
2023-04-23 14:58:35 +0100

    falling rectangule with increased falling speed and arrow control keys

--- components.js
+++ components.js
@@ -3,9 +3,9 @@ class Ball {
         this.x = 0;
         this.y = 760;
         this.radius = 10;
-        this.speed = Math.floor(Math.random() * 6) + 1;
+        this.speed = Math.floor(Math.random() * 10) + 1;
         setInterval(() => {
-            this.speed = Math.floor(Math.random() * 6) + 1;
+            this.speed = Math.floor(Math.random() * 25) + 1;
         }, 500);
     }
 
@@ -25,3 +25,65 @@ class Ball {
         context.closePath();
     }
 }
+
+class Square {
+    constructor(width, height, x, y, speed) {
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = y;
+      this.speed = speed;
+      this.isResetting = false;
+    }
+  
+    update() {
+      if (this.isResetting) {
+        return;
+      }
+  
+      this.y += this.speed;
+  
+      // check square hiting the bottom of the canvas
+      if (this.y + this.height > canvas.height) {
+        this.speed += 1;
+        
+        //  half a second delay
+        this.isResetting = true;
+        setTimeout(() => {
+          this.reset();
+          setTimeout(() => {
+            this.isResetting = false;
+          }, 500);
+        }, 500);
+      }
+    }
+  
+    reset() {
+      this.x = canvas.width / 2 - 35;
+      this.y = 0;
+    }
+  
+    draw(context) {
+      context.fillStyle = "red";
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  
+    moveLeft() {
+      if (!this.isResetting) {
+        this.x -= 10;
+      }
+    }
+  
+    moveRight() {
+      if (!this.isResetting) {
+        this.x += 10;
+      }
+    }
+  
+    moveDown() {
+      if (!this.isResetting) {
+        this.y += 50;
+      }
+    }
+  }
+  
--- game.js
+++ game.js
@@ -3,7 +3,13 @@ class Game {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
         this.ball = new Ball();
+        this.square = new Square(70, 40, canvas.width / 2 - 35, 0, 2); // create the square
         this.isRunning = false;
+        this.score = 0;
+        this.lifes = 3;
+
+        // listen for arrow keys and down key
+        window.addEventListener("keydown", this.handleKeyDown.bind(this));
     }
 
     start() {
@@ -17,11 +23,35 @@ class Game {
         }
 
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // update and draw the ball
         this.ball.update();
         this.ball.draw(this.context);
+
+        // update and draw the square
+        this.square.update();
+        this.square.draw(this.context);
+
+        // draw the score and lifes
+        this.context.fillStyle = "#fff";
+        this.context.fillRect(20, 20, 140, 40);
+        this.context.font = "20px Arial";
+        this.context.fillStyle = "#333";
+        this.context.fillText(`Score: ${this.score}; Lifes: ${this.lifes}`, 25, 45);
 
         requestAnimationFrame(() => {
             this.animate();
         });
+    }
+
+    handleKeyDown(event) {
+        const keyCode = event.keyCode;
+        if (keyCode === 37) { // left arrow
+            this.square.moveLeft();
+        } else if (keyCode === 39) { // right arrow
+            this.square.moveRight();
+        } else if (keyCode === 40) { // down arrow
+            this.square.moveDown();
+        }
     }
 }
